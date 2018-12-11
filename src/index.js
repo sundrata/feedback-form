1d5e6726949d24c5a984dce8dbdb0107efe2db42
Carson Otto <carsonhockey15@gmail.com>
2018-12-10 18:18:41 -0600

    fixes CRITICAL error in index imports that would not run app

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
-import {createStore, combineReducers, applyMiddleware} from 'redux';cons
+import {createStore, combineReducers, applyMiddleware} from 'redux'
 import { Provider } from 'react-redux';
 import logger from 'redux-logger';
 
